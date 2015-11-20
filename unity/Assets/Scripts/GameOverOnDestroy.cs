51b64d10ac201d7f76cdd1e8e4b8d745da08fd26
Steve Haßenpflug <ryzawy@gmail.com>
2015-11-20 13:58:18 +0100

    Added GameSystem with game over function and level reset

---
+++ unity/Assets/Scripts/GameOverOnDestroy.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverOnDestroy : MonoBehaviour {
+
+    public GameSystem gameSystem;
+
+    void OnDestroy()
+    {
+        gameSystem.isGameOver = true;
+        Debug.Log("ITS GAME OVER MAN");
+
+    }
+}
---
+++ unity/Assets/Scripts/GameSystem.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSystem : MonoBehaviour {
+
+    public bool isGameOver = false;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(isGameOver)
+        {
+            // TODO: GUI stuff 
+
+            if (Input.GetButton("Fire1")) {
+                Application.LoadLevel(0);
+            }
+        }
+
+	}
+}
