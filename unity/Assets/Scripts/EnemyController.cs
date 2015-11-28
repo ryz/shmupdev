1cb1117cc19ac16ba1f14c7fc54df497f9d6a7e4
Steve Haßenpflug <ryzawy@gmail.com>
2015-11-28 17:06:00 +0100

    Enemies now move in sin waves

--- unity/Assets/Scripts/EnemyController.cs
+++ unity/Assets/Scripts/EnemyController.cs
@@ -4,6 +4,12 @@ using System.Collections;
 public class EnemyController : MonoBehaviour {
 
     public float speed = 3.0f;
+    public float sinAmplitude = 0.01f;
+    public float sinFrequency = 0.01f;
+    private float horizontalOffset = 0.0f;
+
+    private float time;
+
     public int health = 2; 
 
 	// Use this for initialization
@@ -13,6 +19,19 @@ public class EnemyController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position -= transform.up * speed * Time.deltaTime;
+        MoveForward(speed);
 	}
+
+    void MoveForward(float _speed)
+    {
+        time += Time.deltaTime;
+        //Moves enemy forward
+        transform.position -= transform.up * _speed * Time.deltaTime;
+
+        //Adjust horizontal pos by sine wave
+        horizontalOffset = Mathf.Sin(time * sinFrequency) * sinAmplitude;
+
+        transform.position += horizontalOffset * transform.right;
+    }
 }
+
